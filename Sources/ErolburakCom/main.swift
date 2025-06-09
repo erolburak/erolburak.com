7284d893448ec3dc97ddd00b56c706f40a7c8134
Burak Erol <140210017+erolburak@users.noreply.github.com>
2025-06-09 16:33:21 +0200

    Update README.md (#41)

--- Sources/ErolburakCom/main.swift
+++ Sources/ErolburakCom/main.swift
@@ -8,11 +8,11 @@
 import Publish
 
 try ErolburakCom().publish(using: [.copyResources(to: "resources"),
-                                  .copyFile(at: Path("resources/CNAME")),
-                                  .copyFile(at: Path("resources/README.md")),
-                                  .addMarkdownFiles(),
-                                  .generateHTML(withTheme: .customTheme),
-                                  .copyDefaultIndexHtml(),
-                                  .deploy(using: .gitHub("erolburak/erolburak.github.io",
-                                                         branch: "main",
-                                                         useSSH: true))])
+                                   .copyFile(at: Path("resources/CNAME")),
+                                   .copyFile(at: Path("resources/README.md")),
+                                   .addMarkdownFiles(),
+                                   .generateHTML(withTheme: .customTheme),
+                                   .copyDefaultIndexHtml(),
+                                   .deploy(using: .gitHub("erolburak/erolburak.github.io",
+                                                          branch: "main",
+                                                          useSSH: true))])
