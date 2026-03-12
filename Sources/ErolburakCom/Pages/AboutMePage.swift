dac1d3e7492a0aaf7c67a3a8ec314d49a50d7199
Burak Erol <140210017+erolburak@users.noreply.github.com>
2026-03-12 22:39:10 +0100

    Update AboutMePage.swift

--- Sources/ErolburakCom/Pages/AboutMePage.swift
+++ Sources/ErolburakCom/Pages/AboutMePage.swift
@@ -33,7 +33,7 @@ struct AboutMePage: StaticPage {
 
                 Text(
                     markdown: language == .en
-                    ? "I'm a Senior iOS Developer from Berlin and have been developing apps since 2016. I received my Bachelor of Science (B.Sc.) degree from the University of Applied Sciences Hof in the Mobile Computing course. My focus in this course was smartphone and tablet programming. That's how I ended up in the Apple cosmos while I was studying, and I've remained loyal to it ever since. You can get an impression of my portfolio [here](https://erolburak.com/en/portfolio)."
+                    ? "I'm a Senior iOS Engineer from Berlin and have been developing apps since 2016. I received my Bachelor of Science (B.Sc.) degree from the University of Applied Sciences Hof in the Mobile Computing course. My focus in this course was smartphone and tablet programming. That's how I ended up in the Apple cosmos while I was studying, and I've remained loyal to it ever since. You can get an impression of my portfolio [here](https://erolburak.com/en/portfolio)."
                     : "Ich bin ein Senior iOS Entwickler aus Berlin und entwickle Apps seit 2016. Den Bachelor of Science (B.Sc.) Abschluss habe ich an der Hochschule für Angewandte Wissenschaften Hof im Studiengang Mobile Computing erlangt. Meine Schwerpunkte in diesem Studiengang lagen in Smartphone- und Tablet-Programmierung. So hat es mich schon im Studium in den Apple Kosmos verschlagen, dem ich seitdem treu geblieben bin. [Hier](https://erolburak.com/de/portfolio) könnt ihr euch einen Eindruck über mein Portfolio verschaffen."
                 )
 
